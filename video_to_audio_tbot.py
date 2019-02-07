a1af0341974f5c60844e4620dbe72be7d99f1c83
qwerity <qwerity@gmail.com>
2019-02-07 13:24:26 +0400

    added uri validator

--- video_to_audio_tbot.py
+++ video_to_audio_tbot.py
@@ -1,6 +1,6 @@
-TOKEN = "784653057:AAGkrGRFtG8jb75t8Iy0sxCoxjcC21u-tRc"
-
 import youtube_dl
+
+from urllib.parse import urlparse
 
 import os, sys
 import asyncio
@@ -9,28 +9,40 @@ import telepot.aio
 from telepot.aio.loop import MessageLoop
 
 
+def uri_validator(x):
+    try:
+        result = urlparse (x)
+        return all ([result.scheme, result.netloc, result.path])
+    except:
+        return False
+
+
 async def on_chat_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    print('Chat:', content_type, chat_type, chat_id)
+    content_type, chat_type, chat_id = telepot.glance (msg)
+    print ('Chat:', content_type, chat_type, chat_id)
 
     if content_type != 'text':
         return
 
-    command = msg['text'][-1:].lower()
+    command = msg['text'][-1:].lower ()
 
     if command == 'c':
-        await bot.sendMessage(chat_id, "puff")
+        await bot.sendMessage (chat_id, "puff")
     else:
         url = msg['text']
+        if not uri_validator (url):
+            await bot.sendMessage (chat_id, "please provide valid url")
+            return
+
         options = {
             'format': 'worstaudio',
             # 'extractaudio': True,  # only keep the audio
             # 'audioformat': "m4a",  # convert to m4a
             'outtmpl': '%(id)s.%(ext)s',  # name the file the ID of the video
-            'noplaylist': True,    # only download single song, not playlist
+            'noplaylist': True,  # only download single song, not playlist
         }
         with youtube_dl.YoutubeDL (options) as ydl:
-            await bot.sendMessage (chat_id, "Please wait till video will be downloaded and uploaded to you back ;)")
+            await bot.sendMessage (chat_id, "Please wait till video will be downloaded and uploaded back to you)")
 
             info_dict = ydl.extract_info (url, download=True)
             title = info_dict.get ('title', None)
@@ -43,20 +55,20 @@ async def on_chat_message(msg):
             os.unlink (id + '.' + ext)
 
             with open (converted_audio_file, 'rb') as f:
-                await bot.sendDocument(chat_id, f, caption=title, reply_to_message_id=msg['message_id'])
+                await bot.sendDocument (chat_id, f, caption=title, reply_to_message_id=msg['message_id'])
 
-            os.unlink(converted_audio_file)
+            os.unlink (converted_audio_file)
 
-if 2 != len(sys.argv):
-    print("Please provide Telegram bot token as an only argument")
-    exit(1)
 
-bot = telepot.aio.Bot(str(sys.argv[1]))
-# bot.deleteWebhook()
-answerer = telepot.aio.helper.Answerer(bot)
+if 2 != len (sys.argv):
+    print ("Please provide Telegram bot token as an only argument")
+    exit (1)
 
-loop = asyncio.get_event_loop()
-loop.create_task(MessageLoop(bot, {'chat': on_chat_message}).run_forever())
-print('Listening ...')
+bot = telepot.aio.Bot (str (sys.argv[1]))
+answerer = telepot.aio.helper.Answerer (bot)
 
-loop.run_forever()
+loop = asyncio.get_event_loop ()
+loop.create_task (MessageLoop (bot, {'chat': on_chat_message}).run_forever ())
+print ('Listening ...')
+
+loop.run_forever ()
